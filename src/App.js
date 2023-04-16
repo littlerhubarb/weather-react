88d940ea0fb3e4897fbf21ba6628d80c89d8d04c
littlerhubarb <109243033+littlerhubarb@users.noreply.github.com>
2023-04-16 20:11:17 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
      <h1>Weather app</h1>
      <SearchEngine />
+     <footer>
+       This page is created by YR and is <a href='https://github.com/littlerhubarb/weather-react'>open-sourced</a>
+     </footer>
     </div>
   );
 }
